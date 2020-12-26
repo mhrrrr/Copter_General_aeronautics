79c220266efd474183fe4e73a67029748578afd4
Sachchit Vekaria (laptop) <sachchit.vekaria@generalaeronautics.com>
2020-12-26 10:31:48 +0530

    CA04 Configuration added

--- libraries/AP_Motors/AP_MotorsMatrix.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix.cpp
@@ -468,6 +468,15 @@ void AP_MotorsMatrix::setup_motors(motor_frame_class frame_class, motor_frame_ty
                     add_motor(AP_MOTORS_MOT_6,-150, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  4);
                     success = true;
                     break;
+                case MOTOR_FRAME_TYPE_CA04:
+                	add_motor_raw(AP_MOTORS_MOT_1, 0, 0.765, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 1);
+                	add_motor_raw(AP_MOTORS_MOT_2, 0, -0.765, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 4);
+                	add_motor_raw(AP_MOTORS_MOT_3, 0.8287, -0.4443, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  5);
+                	add_motor_raw(AP_MOTORS_MOT_4, -0.8287, 0.4443, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 2);
+                	add_motor_raw(AP_MOTORS_MOT_5, 0.8287, 0.4443, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 6);
+                	add_motor_raw(AP_MOTORS_MOT_6, -0.8287, -0.4443, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 3);
+					success = true;
+                    break;
                 case MOTOR_FRAME_TYPE_ASYMM_X:
                 	add_motor_raw(AP_MOTORS_MOT_1, -1, 0, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  2);
                 	add_motor_raw(AP_MOTORS_MOT_2, 1, 0, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 5);
